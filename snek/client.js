0d4a1686c939ada524c51aa5652f59e3d4227566
Owen Ripley <ojripley@gmail.com>
2019-09-25 12:55:41 -0400

    created the client.js file and moved the contents of play.js to it

---
+++ snek/client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.46.2.204',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  // recieves data from the server, logs it to our console
+  conn.on('data', (data) => {
+    console.log('', data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
